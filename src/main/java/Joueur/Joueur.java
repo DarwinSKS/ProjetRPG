48137034ab1e7cc438a5046757d51f596ded9760
matt <matteo.deregnaucourt.etude@gmail.com>
2025-03-04 14:41:59 +0100

    upadate joueur + testjoueur

--- src/main/java/Joueur/Joueur.java
+++ src/main/java/Joueur/Joueur.java
@@ -6,7 +6,7 @@ public class Joueur extends Classes.Classes {
     private String nomClasse;
     private String nomJoueur;
 
-    public Joueur(String nom, String classeNom, int pv, int pm, int force, int intelligence, int def, int resMagique, int agilite, int chance, int end) {
+    public Joueur(String nom, String classeNom, int pv, int pm, int force, int intelligence, int def, int resMagique, int agilite, int chance, int end, int esprit) {
         super(classeNom, pv, pm, force, intelligence, def, resMagique, agilite, chance, end, esprit);
         this.nomClasse = nomClasse;
         this.nomJoueur = nomJoueur;
