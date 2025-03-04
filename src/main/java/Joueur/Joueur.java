20299951e799d1350f69ada64baf4277f28a7f61
MathysF59 <franchomme.mathys@outlook.fr>
2025-03-04 14:30:48 +0100

    init main

--- src/main/java/Joueur/Joueur.java
+++ src/main/java/Joueur/Joueur.java
@@ -12,21 +12,7 @@ public class Joueur extends Classes.Classes {
         this.nomJoueur = nomJoueur;
     }
 
-    public String getNom() {
-        return nomClasse;
-    }
-
-    public void setNom(String nom) {
-        this.nomClasse = nomClasse;
-    }
-
-    public String getNomJoueur() {
-        return nomJoueur;
-    }
-
-    public void setNomJoueur(String nomJoueur) {
-        this.nomJoueur = nomJoueur;
-    }
+    public Joueur() {}
 
     public void choisirNomJoueur() {
         Scanner sc = new Scanner(System.in);
